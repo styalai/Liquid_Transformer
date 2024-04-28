1a97061f3b36700b03c6c891a57d6f02506cba41
styalai <arthur.lagacherie@icloud.com>
2024-04-28 12:35:12 +0200

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Liquid_Transformer',
+    url='https://github.com/styalai/Liquid_Transformer',
+    author='Styalai',
+    install_requires=['torch'],
+    version='0.1',
+    license='MIT',
+    description='A package for easy to use gliquid transformer',
+)
